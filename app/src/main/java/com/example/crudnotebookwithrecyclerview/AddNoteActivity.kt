96a74700b5572553a1fec827a5f0bbcc589d712c
engmercy <marcynyaata@gmail.com>
2023-05-26 14:48:35 +0300

    added intention for data reception to AddNoteActivity

--- app/src/main/java/com/example/crudnotebookwithrecyclerview/AddNoteActivity.kt
+++ app/src/main/java/com/example/crudnotebookwithrecyclerview/AddNoteActivity.kt
@@ -1,12 +1,55 @@
 package com.example.crudnotebookwithrecyclerview
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class AddNoteActivity : AppCompatActivity() {
+    lateinit var titleEditText:EditText
+    lateinit var descEditText: EditText
+    lateinit var cancelBtn:Button
+    lateinit var addNoteBtn:Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_note)
         supportActionBar?.title="Add Note"
+
+        titleEditText=findViewById(R.id.titleNoteEditText)
+        descEditText=findViewById(R.id.descNoteEditText)
+        cancelBtn=findViewById(R.id.cancelBtn)
+        addNoteBtn=findViewById(R.id.addNoteBtn)
+
+        addNoteBtn.setOnClickListener {
+            addNote()
+        }
+        cancelBtn.setOnClickListener {
+            cancelNote()
+        }
+    }
+    private fun addNote(){
+        val title=titleEditText.text.toString()
+        val desc=descEditText.text.toString()
+        val intent=Intent()
+        if(title=="" && desc==""){
+            Toast.makeText(this,"You need at least one input",Toast.LENGTH_SHORT).show()
+        }
+        else{
+            intent.putExtra("title",title)
+            intent.putExtra("desc",desc)
+            setResult(RESULT_OK,intent)
+            finish()
+        }
+
+    }
+    private fun cancelNote(){
+        Toast.makeText(this,"Note cancelled",Toast.LENGTH_SHORT).show()
+        finish()
+
+
     }
 }
--- app/src/main/java/com/example/crudnotebookwithrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/crudnotebookwithrecyclerview/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.crudnotebookwithrecyclerview
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.crudnotebookwithrecyclerview.adapters.CustomNoteAdapter
@@ -15,6 +20,8 @@ class MainActivity : AppCompatActivity() {
     var imageArray= arrayListOf<Int>(R.drawable.me,R.drawable.me)
     var descArray= arrayListOf<String>("This is note one","This is note two")
     lateinit var notesRecyclerView:RecyclerView
+//    get data from AddNoteActivity
+    lateinit var addActivityResultLauncher:ActivityResultLauncher<Intent>
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +32,7 @@ class MainActivity : AppCompatActivity() {
         notesRecyclerView.layoutManager=LinearLayoutManager(this@MainActivity)
         notesRecyclerView.adapter=CustomNoteAdapter(titleArray,descArray,imageArray,this)
 
+        getResponseDataFromAddNoteActivity()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -35,13 +43,59 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         super.onOptionsItemSelected(item)
         if(item.itemId==R.id.addNote){
-            TODO("write add note functionality")
+            val intent=Intent(this,AddNoteActivity::class.java)
+            addActivityResultLauncher.launch(intent)
 
         }
         else if(item.itemId==R.id.deleteNotes){
             // TODO: write delete notes functionalities
         }
         return false
+
+    }
+   private fun getResponseDataFromAddNoteActivity(){
+       addActivityResultLauncher=registerForActivityResult(ActivityResultContracts.StartActivityForResult(),
+           ActivityResultCallback {
+           resultAddNote->
+           val resCode=resultAddNote.resultCode
+           val data=resultAddNote.data
+           if(resCode== RESULT_OK && data!=null){
+               val title=data.getStringExtra("title")
+               val desc=data.getStringExtra("desc")
+              if(title=="" && desc!=""){
+                   titleArray.add("No title")
+                   imageArray.add(R.drawable.me)
+                   if (desc != null) {
+                       descArray.add(desc)
+                   }
+                   notesRecyclerView.adapter=CustomNoteAdapter(titleArray,descArray,imageArray,this)
+
+               }else if(title!="" && desc==""){
+                   if (title != null) {
+                       titleArray.add(title)
+                   }
+                   imageArray.add(R.drawable.me)
+                   descArray.add("No desc")
+                   notesRecyclerView.adapter=CustomNoteAdapter(titleArray,descArray,imageArray,this)
+               }
+               else  if(title=="" ||desc==""){
+                  titleArray.add("No title")
+                  descArray.add("No desc")
+                  imageArray.add(R.drawable.me)
+                  notesRecyclerView.adapter=CustomNoteAdapter(titleArray,descArray,imageArray,this)
+              }
+               else{
+                   imageArray.add(R.drawable.me)
+                   if (title != null) {
+                       titleArray.add(title)
+                   }
+                   if (desc != null) {
+                       descArray.add(desc)
+                   }
+                   notesRecyclerView.adapter=CustomNoteAdapter(titleArray,descArray,imageArray,this)
+               }
+           }
+       })
 
     }
 }
